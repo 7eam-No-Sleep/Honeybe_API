791ca63c75d4d615087d96477b4f7674b9bf0fd8
tleblanc302 <tpossusa@gmail.com>
2024-04-21 02:34:40 -0500

    -- Still Testing -- Added Export Controller to export the desired report from our db to a csv file and added a route that returns said file to the frontend.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExportController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/api/export', [ExportController::class, 'export']);
